722dd37878d41ed89cf169b1d50a252803572b0f
권민 <minkwon4@naver.com>
2020-09-09 17:43:39 +0900

    UPDATE CrawlingService
    Former-commit-id: 5adf91f1a26ef28f758e78ddfc222c85d4ff70df

--- backend/src/main/java/com/bigdata/tikitacar/crawling/service/CrawlingServiceImpl.java
+++ backend/src/main/java/com/bigdata/tikitacar/crawling/service/CrawlingServiceImpl.java
@@ -24,10 +24,11 @@ public class CrawlingServiceImpl implements CrawlingService {
             bufferedWriter = Files.newBufferedWriter(Paths.get("C:\\Users\\one10004\\Desktop\\data.csv"), Charset.forName("UTF-8"));
             initCsv(bufferedWriter);
 
-            for (int i = 17023; i <= 17023; i++) {
+            int no=1;
+            for (int i = 17023; i <= 17023; i++,no++) {
                 url = "https://certifiedcar.hyundaicapital.com/hcsfront/ms/carView?xc_vcl_cd=HCL" + i;
                 document = Jsoup.connect(url).get();
-                addToCsv(document, bufferedWriter);
+                addToCsv(no,document, bufferedWriter);
             }
 
             bufferedWriter.close();
@@ -76,13 +77,52 @@ public class CrawlingServiceImpl implements CrawlingService {
     }
 
     @Override
-    public void addToCsv(Document html, BufferedWriter bufferedWriter) throws Exception {
+    public void addToCsv(int no,Document html, BufferedWriter bufferedWriter) throws Exception {
         Element element = null;
         String text = null;
+
+        //인덱스
+        bufferedWriter.write(no);
+        bufferedWriter.write(",");
+
+        //차이름
         element = html.select("dl.car_info dt").first();
         text = element.text();
         int idx = text.indexOf("등급");
         text = text.substring(0, idx - 1);
-        System.out.println(text);
+        bufferedWriter.write(text);
+        bufferedWriter.write(",");
+
+        //상세이름
+
+        //가격
+
+        //연식
+
+        //주행거리
+
+        //배기량관리
+
+        //색상
+
+        //변속기
+
+        //연료
+
+        //인승
+
+        //신차가격
+
+        //압류,저당
+
+        //옵션총개수
+
+        //보험
+
+        //관리이력
+
+        //침수이력
+
+        bufferedWriter.newLine();
     }
 }
