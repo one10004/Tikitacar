144ef8332c481b06a54556702e37cd59007e3af0
맹승규 <aodtmdrb@naver.com>
2020-09-10 15:25:18 +0900

    Update vue project directory structure, S03P22A106-38
    Former-commit-id: 289cf31cb9acffebe001fb57ed4950a6d05d3d81

--- frontend/tikitacar/src/App.vue
+++ frontend/tikitacar/src/App.vue
@@ -1,17 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      right
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="cyan"
+      dark
+    >
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>tikitaCAR!</v-toolbar-title>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-main>
+      <router-view :key="$route.fullPath" />
+    </v-main>
+
+    <v-footer
+      color="cyan"
+      app
+    >
+      <v-spacer></v-spacer>
+
+      <span class="white--text">&copy; {{ new Date().getFullYear() }} tikitacar</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  props: {
+      source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
