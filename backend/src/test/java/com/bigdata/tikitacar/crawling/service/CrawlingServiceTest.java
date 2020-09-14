0b37c586e919ea91f7fddfbfd34cc51fc37e8249
boo <wjdtn7823@naver.com>
2020-09-14 15:26:29 +0900

    test with jsse.enableSNIExtension
    Former-commit-id: 9b84c2eb0f8cf67ad1d2d86d78c50ac8d3adab46

--- backend/src/test/java/com/bigdata/tikitacar/crawling/service/CrawlingServiceTest.java
+++ backend/src/test/java/com/bigdata/tikitacar/crawling/service/CrawlingServiceTest.java
@@ -1,10 +1,13 @@
 package com.bigdata.tikitacar.crawling.service;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+import javax.net.ssl.SSLHandshakeException;
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,8 +19,25 @@ class CrawlingServiceTest {
     @Autowired
     CrawlingService crawlingService;
 
-    @Test
+    @DisplayName("false 로 실행")
+    @RepeatedTest(10)
     public void crawl(){
-        crawlingService.crawlingCarHtml();
+
+        System.setProperty("jsse.enableSNIExtension", "false");
+        assertDoesNotThrow(()->{
+            crawlingService.crawlingCarHtml();
+
+        });
+    }
+
+    @DisplayName("true 로 실행")
+    @RepeatedTest(10)
+    public void crawl2(){
+        System.setProperty("jsse.enableSNIExtension", "true");
+
+        assertDoesNotThrow(()->{
+            crawlingService.crawlingCarHtml();
+
+        });
     }
 }
