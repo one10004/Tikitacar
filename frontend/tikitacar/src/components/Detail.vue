ff6443d20d9ce9f4f72d13cc5ac95a716cce7e73
맹승규 <aodtmdrb@naver.com>
2020-09-15 02:28:24 +0900

    Update detail page, S03P22A106-56
    Former-commit-id: 0170033b11b126f6d903ca091df7abf5aa699313

--- frontend/tikitacar/src/components/Detail.vue
+++ frontend/tikitacar/src/components/Detail.vue
@@ -1,15 +1,44 @@
 <template>
   <v-app id="inspire">
-    <v-main style="padding-left: 5%;">
-      <h3>Detail page</h3>
-      <div class="mainImage">
-        Image
-      </div>
-      <div class="subImages">
-        <div class="subImage1">sub image1</div>
-        <div class="subImage1">sub image2</div>
-        <div class="subImage1">sub image3</div>
-        <div class="subImage1">sub image4</div>
+    <v-main style="padding-left: 5%; margin-top: 50px;">
+      <h1>Detail page</h1>
+      <h3>Car name</h3>
+      <div class="carInfo">
+        <div>
+          <div class="mainImage">
+            Image
+          </div>
+          <div class="subImages">
+            <div class="subImage1">sub image1</div>
+            <div class="subImage1">sub image2</div>
+            <div class="subImage1">sub image3</div>
+            <div class="subImage1" style="margin-right: 0px;">sub image4</div>
+          </div>
+        </div>
+        <div>
+          <div class="spec">
+            <h4>모델: </h4>
+            <v-text-field 
+              readonly
+              v-model="model"
+            ></v-text-field>
+          </div>
+          <div class="spec">
+            <h4>연식: </h4>
+            <v-text-field 
+              readonly
+              v-model="year"
+            ></v-text-field>
+          </div>
+          <div class="spec">
+            <h4>주행거리: </h4>
+            <v-text-field 
+              readonly
+              v-model="km"
+              suffix="km"
+            ></v-text-field>
+          </div> 
+        </div>     
       </div>
     </v-main>
   </v-app>
@@ -20,6 +49,11 @@
     props: {
       source: String,
     },
+    data: () => ({
+      model: 'Tesla Model 3',
+      year: 2019,
+      km: "8,000"
+    })
   }
 </script>
 
@@ -28,20 +62,37 @@
     margin-top: 50px;
     margin-left: 8px;
   }
+  h4 {
+    margin: 10px;
+    margin-top: 20px;
+    width: 100px;
+  }
+  .carInfo {
+    display: flex;
+  }
   .mainImage {
+    line-height: 350px;
     text-align: center;
-    width: 500px;
-    height: 450px;
+    width: 450px;
+    height: 350px;
     border: solid;
     
   }
   .subImages {
     display: flex;
+    width: 450px;
   }
   .subImage1 {
-    margin: 5px 20px 5px 0px;
-    width: 110px;
-    height: 90px;
+    line-height: 75px;
+    text-align: center;
+    margin: 5px 19px 5px 0px;
+    width: 98px;
+    height: 80px;
     border: solid;
+  }
+  .spec {
+    margin-left: 100px;
+    margin-top: 10px;
+    display: flex;
   }
 </style>
