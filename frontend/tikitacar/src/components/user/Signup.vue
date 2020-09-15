23ba1cbb45905f398a6e24fda5a6bf1dcabe1ebf
boo <wjdtn7823@naver.com>
2020-09-15 14:54:24 +0900

    회원 입력 기본 틀 완성(Todo : vuex 로 변경, css, 백엔드 api 연결)
    Former-commit-id: 976bfb673f79b30efe3305534ca2285ac9dfee7b

---
+++ frontend/tikitacar/src/components/user/Signup.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-app id="app">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row justify="start">
+
+          <v-col lg6>
+            <v-card class="elevation-20">
+              <v-card-title>
+                TIKITACAR LOGO
+              </v-card-title>
+              <v-form>
+                <v-text-field
+                    label="이름"
+                    name="name"
+                    id ="name"
+                    type="text"
+                ></v-text-field>
+                <v-text-field
+                    label="Email"
+                    name="email"
+                    id ="email"
+                    type="text"
+
+                ></v-text-field>
+                  <v-text-field
+                      label="비밀번호"
+                      name="pw"
+                      id ="pw"
+                      type="password"
+
+                      append-icon="mdi-lock"
+
+                  ></v-text-field>
+                    <v-text-field
+                        label="비밀번호 확인"
+                        name="pwConfirm"
+                        id ="pwConfirm"
+                        type="password"
+                        append-icon="mdi-lock"
+
+                    >
+                </v-text-field>
+                <v-text-field
+                    label="생년월일(YYYYMMDD)"
+                    name="dob"
+                    id ="dob"
+                    type="text"
+                >
+                </v-text-field>
+
+                <v-select
+                    label="성별"
+                :items="['여자', '남자']"
+                >
+
+                </v-select>
+                <v-text-field
+                    label="핸드폰번호(0000000000)"
+                    name="dob"
+                    id ="dob"
+                    type="text"
+                >
+                </v-text-field>
+
+                <v-text-field
+                    label="주소"
+                    name="dob"
+                    id ="dob"
+                    :placeholder="address"
+                    :readonly = true
+                    type="text"
+                    v-on:click="stop = true"
+                >
+                </v-text-field>
+                <v-text-field
+                    label="상세주소"
+                    name="dob"
+                    id ="dob"
+                    type="text"
+                >
+                </v-text-field>
+              </v-form>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="cyan" >회원가입</v-btn>
+                <v-dialog
+                v-model="stop">
+                  <vue-daum-postcode @complete="handleAddress"></vue-daum-postcode>
+                </v-dialog>
+               </v-card-actions>
+            </v-card>
+          </v-col>
+         =
+        </v-row>
+
+     </v-container>
+   </v-main>
+
+  </v-app>
+
+</template>
+
+
+
+<script>
+
+export default{
+  data(){
+    return {
+      stop : false,
+      result : "",
+      address : ""
+    }
+  },
+  methods : {
+    handleAddress : function(data){
+      this.address = data.address;
+      this.stop = false;
+    }
+  }
+}
+
+
+
+
+</script>
+
+
+<style>
+
+
+</style>
