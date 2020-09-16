1d744e3af7d53259f409ea73db2828580175159c
권민 <minkwon4@naver.com>
2020-09-16 09:49:46 +0900

    Update models.py
    Former-commit-id: f10ff785d2da7a0b281e109b6ac99e62a1152204

--- django/predictcar/predict/models.py
+++ django/predictcar/predict/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Car(models.Model):
-    year = models.CharField(max_legnth=10)
-    cc = models.CharField(max_legnth=10)
-    distance = models.CharField(max_legnth=10)
+    year = models.IntegerField()
+    cc = models.IntegerField()
+    distance = models.IntegerField()
     color = models.CharField(max_legnth=10)
-    gear = models.CharField(max_legnth=10)
-    fuel = models.CharField(max_legnth=10)
-    option = models.CharField(max_legnth=10)
-    seat = models.CharField(max_legnth=10)
-    flooding = models.CharField(max_legnth=10)
-    insurance = models.CharField(max_legnth=10)
-    release = models.CharField(max_legnth=10)
+    gear = models.harField(max_length=10)
+    fuel = models.CharField(max_length=10)
+    option = models.IntegerField()
+    seat = models.IntegerField()
+    flooding = models.IntegerField()
+    insurance = models.IntegerField()
+    release = models.IntegerField()
 
     def __str__self():
         return self.title
