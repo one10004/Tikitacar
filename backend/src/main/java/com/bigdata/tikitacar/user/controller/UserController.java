9a1583d02048f4df67d7cad0b0155c3bcf27e169
Choi Won Dae <ryunhoo123@kookmin.ac.kr>
2020-09-17 12:08:39 +0900

    Add Swagger Operation
    Swagger를 위한 기초
    
    
    Former-commit-id: 855552151ba961b195e431c12a184f22171cbcb7

--- backend/src/main/java/com/bigdata/tikitacar/user/controller/UserController.java
+++ backend/src/main/java/com/bigdata/tikitacar/user/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.bigdata.tikitacar.user.controller;
 
+import com.bigdata.tikitacar.user.dto.request.UserRegisterRequestDto;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/user")
 public class UserController {
+
+    @ApiOperation("회원가입")
+    @PostMapping("")
+    public Object signup(@RequestBody UserRegisterRequestDto userRegisterRequestDto){
+        return null;
+    }
+
+    @ApiOperation("회원 정보 불러오기")
+    @GetMapping("")
+    public Object getUserInfo(@RequestHeader(value="Authorization") String token){
+        return null;
+    }
+
+    @ApiOperation("회원 정보 업데이트(수정)")
+    @PutMapping("/{id}")
+    public Object updateUserInfo(@PathVariable("id") Long id){
+        return null;
+    }
+
+    @ApiOperation("회원 탈퇴")
+    @DeleteMapping("/{id}")
+    public Object deleteUser(@PathVariable("id") Long id){
+        return null;
+    }
+
+    @ApiOperation("이메일 중복체크")
+    @GetMapping("/email/{email}")
+    public Object checkEmail(@PathVariable("email") String email){
+        return null;
+    }
+
+    @ApiOperation("닉네임 중복체크")
+    @GetMapping("/nickname/{nickname}")
+    public Object checkNickname(@PathVariable("nickname") String nickname){
+        return null;
+    }
 }
--- backend/src/main/java/com/bigdata/tikitacar/user/dto/request/UserRegisterRequestDto.java
+++ backend/src/main/java/com/bigdata/tikitacar/user/dto/request/UserRegisterRequestDto.java
@@ -1,4 +1,23 @@
 package com.bigdata.tikitacar.user.dto.request;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserRegisterRequestDto {
+    private Long id;
+    private String password;
+    private String email;
+    private String nickname;
+    private Date birth;
+    private String gender;
+    private String phone;
+    private String address;
+    private String address_detail;
+    private int auth;
 }
