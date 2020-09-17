6f57c0c65843f294809d906bf8fbe68fe7b1fc57
boo <wjdtn7823@naver.com>
2020-09-17 14:09:21 +0900

    fixed url typeo
    Former-commit-id: 936086b13ac48fde6990e12602925fa61e1f8c63

--- frontend/tikitacar/src/api/api.js
+++ frontend/tikitacar/src/api/api.js
@@ -3,16 +3,16 @@ export default {
     ROOT_URL : process.env.TIKITACAR?"j3a106.p.ssafy.io/" :"http://localhost:3080/",
     ROUTES : {
         USERS: {
-            emailDuplicateCheckRequest: "/user/email/",
-            nicknameDuplicateCheckRequest: "/user/nickname/",
-            getUserInfoRequest : "/user",
-            createUserRequest : "/user",
-            updateUserRequest : "/user",
-            deleteUserRequest : "/user",
+            emailDuplicateCheckURL: "user/email",
+            nicknameDuplicateCheckURL: "user/nickname",
+            getUserInfoURL : "user/",
+            createUserURL : "user",
+            updateUserURL : "user",
+            deleteUserURL : "user",
         },
         AUTH : {
-            loginRequest : "/auth/login",
-            logoutRequest : "/auth/logout"
+            loginURL : "auth/login",
+            logoutRequest : "auth/logout"
         }
     }
 
