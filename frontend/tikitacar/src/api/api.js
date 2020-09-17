bfe483142fcde9b9a569bc65a4943328e23d4022
boo <wjdtn7823@naver.com>
2020-09-17 12:58:30 +0900

    add URL info for user and auth
    Former-commit-id: 4bf646a0cc5f3c2f4a37c54eab4eef7a7d8fd19f

--- frontend/tikitacar/src/api/api.js
+++ frontend/tikitacar/src/api/api.js
@@ -1,6 +1,19 @@
 
 export default {
     ROOT_URL : process.env.TIKITACAR?"j3a106.p.ssafy.io/" :"http://localhost:3080/",
-
+    ROUTES : {
+        USERS: {
+            emailDuplicateCheckRequest: "/user/email/",
+            nicknameDuplicateCheckRequest: "/user/nickname/",
+            getUserInfoRequest : "/user",
+            createUserRequest : "/user",
+            updateUserRequest : "/user",
+            deleteUserRequest : "/user",
+        },
+        AUTH : {
+            loginRequest : "/auth/login",
+            logoutRequest : "/auth/logout"
+        }
+    }
 
 }
