351b1a36d5ce7dcb0d14723711e30e2023843795
boo <wjdtn7823@naver.com>
2020-09-22 11:17:42 +0900

    add expiredTime to token
    Former-commit-id: 4f8084f0983cfdb8bddb24fb2bd61c823605bd1e

--- frontend/tikitacar/src/vuex/mutations.js
+++ frontend/tikitacar/src/vuex/mutations.js
@@ -5,6 +5,10 @@ import cookies from 'vue-cookies'
 export default {
     SET_USER(state, res) {
         cookies.set('auth-token', res.data.token);
+        let now = new Date();
+        now.setSeconds(now.getSeconds() + res.data.validTime/1000);
+        cookies.config('auth-token', now);
+        //cookies.set('auth-token', res.data.validTime/1000);
         // state.userInfo.authToken = res.headers.authorization
         // state.userInfo.userNickname = res.data.loginUser.nickName
         // state.userInfo.userCoin = res.data.loginUser.coin
