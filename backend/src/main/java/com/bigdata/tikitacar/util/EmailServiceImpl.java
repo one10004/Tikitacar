9bb04fbc4b82358ab94a63807fad25b922ed7531
Choi Won Dae <ryunhoo123@kookmin.ac.kr>
2020-09-22 14:13:03 +0900

    Modify Email
    Async로 이메일 보내게 변경
    
    
    Former-commit-id: 756bcf91c51d2fc1df05d9df1de658c5072b9b67

--- backend/src/main/java/com/bigdata/tikitacar/util/EmailServiceImpl.java
+++ backend/src/main/java/com/bigdata/tikitacar/util/EmailServiceImpl.java
@@ -3,32 +3,52 @@ package com.bigdata.tikitacar.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Service
-public class EmailServiceImpl implements EmailService{
+public class EmailServiceImpl implements EmailService {
 
     @Autowired
     public JavaMailSender emailSender;
 
+    @Async
+    @Override
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage msg = new SimpleMailMessage();
 
-        try {
-            MimeMessage mimeMessage = emailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true,"utf-8");
+//            MimeMessage mimeMessage = emailSender.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true,"utf-8");
+//
+//            helper.setTo(to);
+//            helper.setSubject(subject);
+//            helper.setText(text,true);
+//
+//            emailSender.send(mimeMessage);
+        msg.setFrom("Tikitacar");
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(text);
 
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text,true);
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setProtocol("smtp");
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("tikitacar106@gmail.com");
+        mailSender.setPassword("ssafy@106");
 
-            emailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+        Properties prop = new Properties();
+        prop.setProperty("mail.smtp.auth", "true");
+        prop.setProperty("mail.smtp.starttls.enable", "true");
+
+        mailSender.setJavaMailProperties(prop);
+        mailSender.send(msg);
+
     }
 }
