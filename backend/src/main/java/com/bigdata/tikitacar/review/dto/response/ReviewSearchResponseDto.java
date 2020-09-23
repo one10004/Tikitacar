0c5766548de1526dd35f952578b7a76084a29563
장윤정 <wkddbswjd325@gmail.com>
2020-09-23 11:04:13 +0900

    Update ReviewSearchResponseDto.java
    Former-commit-id: ca8372141b094a6bdb55a40aa0dc0fd6c010a5f6

--- backend/src/main/java/com/bigdata/tikitacar/review/dto/response/ReviewSearchResponseDto.java
+++ backend/src/main/java/com/bigdata/tikitacar/review/dto/response/ReviewSearchResponseDto.java
@@ -1,5 +1,6 @@
 package com.bigdata.tikitacar.review.dto.response;
 
+import com.bigdata.tikitacar.car.dto.response.DealSearchResponseDto;
 import com.bigdata.tikitacar.car.entity.Deal;
 import com.bigdata.tikitacar.user.entity.User;
 import lombok.*;
@@ -14,8 +15,15 @@ import java.time.LocalDateTime;
 @ToString
 public class ReviewSearchResponseDto {
     private Long id;
+
+    //유저정보
+    String email;
+    String nickname;
+    
+    //거래정보
     private Deal deal;
-    private User writer;
+
+    //후기정보
     private String title;
     private String content;
     private LocalDateTime date;
