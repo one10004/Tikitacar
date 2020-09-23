8e5ae56faf9488089e39f207619468afac1d6383
장윤정 <wkddbswjd325@gmail.com>
2020-09-23 14:47:38 +0900

    Update ReviewService.java
    Former-commit-id: 1fd688a9082b08e6c6a01596a85e92ff935bc819

--- backend/src/main/java/com/bigdata/tikitacar/review/service/ReviewService.java
+++ backend/src/main/java/com/bigdata/tikitacar/review/service/ReviewService.java
@@ -1,10 +1,10 @@
 package com.bigdata.tikitacar.review.service;
 
 import com.bigdata.tikitacar.review.dto.request.ReviewRegisterRequestDto;
+import com.bigdata.tikitacar.review.dto.request.ReviewUpdateRequestDto;
 import com.bigdata.tikitacar.review.dto.response.ReviewSearchResponseDto;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Map;
 
 public interface ReviewService {
@@ -19,7 +19,7 @@ public interface ReviewService {
     public ReviewSearchResponseDto searchReview(Long id);
 
     //Update
-    public void updateReview(ReviewRegisterRequestDto reviewRegisterRequestDto);
+    public void updateReview(ReviewUpdateRequestDto reviewUpdateRequestDto);
 
     //Delete
     public void removeReview(Long id);
