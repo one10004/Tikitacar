0db9695249b069055d2a2327e96c2dd68c75c60b
맹승규 <aodtmdrb@naver.com>
2020-09-24 17:20:20 +0900

    Merge frontend-feature-register into frontend
    Former-commit-id: eb5c2b349c8989a839d68f3bc1db164c1b4abc6b

--- frontend/tikitacar/src/vuex/getters.js
+++ frontend/tikitacar/src/vuex/getters.js
@@ -2,6 +2,7 @@
 
 export default {
     isLoggedIn: state => !!state.userInfo.authToken,
+    getAuthToken : state => state.userInfo.authToken,
     config: state => ({
         headers : {
             "Authorization" : "Bearer" + " " + `${state.userInfo.authToken}`
