ae2f65b5639115a3c77734adb6617e0152151af4
맹승규 <aodtmdrb@naver.com>
2020-10-07 17:18:33 +0900

    Update predict page UI/UX, S03P23A106-76
    Former-commit-id: d04d83107010150848842386007e4554276c174b

--- frontend/tikitacar/src/components/car/Predict.vue
+++ frontend/tikitacar/src/components/car/Predict.vue
@@ -1,31 +1,48 @@
 <template>
   <v-app id="app">
-    <v-main style="padding-left: 10%; margin-top: 50px;">
-      <h3>내 차 가격 조회</h3>
-      <v-row style="margin-right: 100px;">
+    <v-main style="padding-left: 10%; margin-top: 30px;">
+      <p style="font-size: 40px;"><span style="color: blue;">고객</span>님의</p>
+      <p style="font-size: 40px;">차량정보를 입력하고</p>
+      <p style="font-size: 40px;"><span style="color: blue;">빅데이터 기반</span>의 시세를 알아보세요.</p>
+      <h2 style="margin-top: 20px;">Step 1.</h2>
+      <p>모델 입력</p>
+      <v-row>
         <v-col cols="12" sm="2">
-          <h4>모델명</h4>
+          <h4>제조사</h4>
+          <v-select
+            :items="manufacturerOptions"
+            label="제조사"
+            solo
+            @change="selectManufactuer($event)"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <h4>모델</h4>
+          <v-select
+            v-model="model"
+            :items="modelOptions"
+            label="모델"
+            solo
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <h4>세부 모델명</h4>
           <v-text-field
-            placeholder="ex) 테슬라 모델 S"
+            placeholder="ex) 100D"
             style="width: 100%;"
             v-model="info.name"
           ></v-text-field>
         </v-col>
+      </v-row>
+      <h2 style="margin-top: 20px;">Step 2.</h2>
+      <p>기본정보 입력</p>
+      <v-row>
         <v-col cols="12" sm="2">
           <h4>연식</h4>
           <v-text-field
             placeholder="ex) 2018"
             style="width: 100%;"
             v-model="info.year"
-            type="number"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="2">
-          <h4>배기량(cc)</h4>
-          <v-text-field
-            placeholder="ex) 5000"
-            style="width: 100%;"
-            v-model="info.cc"
             type="number"
           ></v-text-field>
         </v-col>
@@ -47,6 +64,19 @@
             v-model="info.color"
             :items="colorOptions"
           ></v-select>
+        </v-col>
+      </v-row>
+      <h2 style="margin-top: 20px;">Step 3.</h2>
+      <p>제원사항 입력</p>
+      <v-row>
+        <v-col cols="12" sm="2">
+          <h4>배기량(cc)</h4>
+          <v-text-field
+            placeholder="ex) 5000"
+            style="width: 100%;"
+            v-model="info.cc"
+            type="number"
+          ></v-text-field>
         </v-col>
         <v-col cols="12" sm="2">
           <h4>기어</h4>
@@ -75,7 +105,11 @@
             type="number"
           ></v-text-field>
         </v-col>
-        <v-col cols="12" sm="2">
+      </v-row>
+      <h2 style="margin-top: 20px;">Step 4.</h2>
+      <p>기타사항 입력</p>
+      <v-row>
+         <v-col cols="12" sm="2">
           <h4>옵션</h4>
           <v-text-field
             placeholder="ex) 1"
@@ -123,7 +157,7 @@
       <div style="display: none" v-if="result === -1">
       </div>
       <div class="result" v-else>
-        <p class="resultSentence">차량의 예상 금액은</p>
+        <p class="resultSentence">빅데이터로 분석한 고객님의 {{model}} {{info.name}} 예상 금액은</p>
         <p class="price">{{this.info.price}} 만원</p>
         <p class="resultSentence">입니다.</p>
         <v-btn color="primary" @click="sellMyCar()">내 차 팔기</v-btn>
@@ -155,12 +189,15 @@ import {mapActions} from "vuex";
         year: Number
       },
       result: -1,
+      model: "",
       colorOptions: ["없음","흰색","검정색","진회색","은색","진주색","회색","베이지색","빨간색","진청색","청색","파란색","하늘색","기타색상"],
       gearOptions: ["없음", "자동", "수동"],
       fuelOptions: ["없음", "가솔린", "디젤", "LPG", "전기"],
+      manufacturerOptions: ["현대", "기아", "쉐보레", "르노삼성", "쌍용"],
+      modelOptions: [],
     }),
     methods: {
-      ...mapActions(["predictPrice"]),
+      ...mapActions(["predictPrice", "getModels"]),
       predict() {
         this.predictPrice(this.info)
           .then((res) => {
@@ -178,7 +215,16 @@ import {mapActions} from "vuex";
             info : this.info,
           }
         });
-      }
+      },
+      selectManufactuer(event) {
+        this.getModels(event)
+          .then((res) => {
+            this.modelOptions = res;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      },
     },
   }
 </script>
@@ -191,7 +237,7 @@ import {mapActions} from "vuex";
     margin-top: 50px;
   }
   h4 {
-    margin-top: 20px;
+    margin-top: 5x;
   }
   .carSearch {
     margin-top: 10px;
